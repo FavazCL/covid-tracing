3f5bd3bcad0d880ab37eeacb9344a31f297df78b
Felipe <favazcl@gmail.com>
2020-08-16 13:05:20 -0400

    Seteo del modo portrait en toda la aplicación, además se creo una vista home para solucionar error

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:covid_app/src/screens/home/home_ui.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,13 +10,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown
+    ]);
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeUI(),
     );
   }
 }
