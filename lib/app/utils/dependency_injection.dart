a8385ad15d2e2d088ce436b8680b46cdab99f74e
Felipe Vergara <favazcl@gmail.com>
2020-10-18 00:18:07 -0300

    add codes injection

--- lib/app/utils/dependency_injection.dart
+++ lib/app/utils/dependency_injection.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:covid_app/app/data/providers/local/handshakes_api.dart';
 import 'package:covid_app/app/data/providers/local/seeds_api.dart';
+import 'package:covid_app/app/data/providers/remote/codes_api.dart';
 import 'package:covid_app/app/data/repositories/local/handshakes_repository.dart';
 import 'package:covid_app/app/data/repositories/local/seeds_repository.dart';
+import 'package:covid_app/app/data/repositories/remote/codes_repository.dart';
 import 'package:get/get.dart';
 
 class DependencyInjection {
@@ -10,9 +12,11 @@ class DependencyInjection {
     // Providers
     Get.put<HandshakesAPI>(HandshakesAPI());
     Get.put<SeedsAPI>(SeedsAPI());
+    Get.put<CodesAPI>(CodesAPI());
 
     // Repositories
     Get.put<HandshakesRepository>(HandshakesRepository());
     Get.put<SeedsRepository>(SeedsRepository());
+    Get.put<CodesRepository>(CodesRepository());
   }
 }
