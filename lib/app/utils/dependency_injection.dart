0e0b8eccc7254ac6c6319a766f0453b1eb6b307b
Felipe Vergara <favazcl@gmail.com>
2020-10-18 00:10:16 -0300

    delete pubspec·

--- lib/app/utils/dependency_injection.dart
+++ lib/app/utils/dependency_injection.dart
@@ -1,8 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:covid_app/app/data/providers/local/handshakes_api.dart';
+import 'package:covid_app/app/data/providers/local/seeds_api.dart';
+import 'package:covid_app/app/data/repositories/local/handshakes_repository.dart';
+import 'package:covid_app/app/data/repositories/local/seeds_repository.dart';
 import 'package:get/get.dart';
 
 class DependencyInjection {
   static void init() {
-    Get.put<FirebaseFirestore>(FirebaseFirestore.instance);
+    // Providers
+    Get.put<HandshakesAPI>(HandshakesAPI());
+    Get.put<SeedsAPI>(SeedsAPI());
+
+    // Repositories
+    Get.put<HandshakesRepository>(HandshakesRepository());
+    Get.put<SeedsRepository>(SeedsRepository());
   }
 }
