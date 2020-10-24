d620509ac1813c59e43034c357e4b01d3953060d
jose.zunigahenriquez@gmail.com <jose.zunigahenriquez@gmail.com>
2020-10-24 12:19:41 -0300

    add ble lib

--- lib/app/modules/scan/scan_controller.dart
+++ lib/app/modules/scan/scan_controller.dart
@@ -1,13 +1,47 @@
+import 'package:covid_app/app/utils/ble/peripheral_tracing.dart';
 import 'package:get/state_manager.dart';
+import 'package:blemulator/blemulator.dart';
+import 'package:flutter_ble_lib/flutter_ble_lib.dart';
 
 class ScanController extends GetxController {
 
-  int value = 0;
+  Blemulator blemulator = Blemulator();
+  BleManager bleManager = BleManager();
+  BleManager bleManager2 = BleManager();
+
+  @override
+  void onClose() {
+    // bleManager2?.stopPeripheralScan();
+    // bleManager2.destroyClient();
+    // bleManager?.destroyClient();
+    stop();
+    super.onClose();
+  }
 
   @override
   void onInit() {
-    print('onInit scan controller');
     super.onInit();
+    initEmulator();
+  }
+
+  stop() async {
+    await bleManager2?.stopPeripheralScan();
+    await bleManager2.destroyClient();
+  }
+
+  void initEmulator() async {
+    // blemulator.addSimulatedPeripheral(PeripheralTracing());
+    // await blemulator.simulate();
+    // await bleManager.createClient();
+
+    await bleManager2.createClient();
+    bleManager2.startPeripheralScan().listen((scanResult) {
+         print("Scanned Peripheral ${scanResult.peripheral.identifier}, RSSI ${scanResult.rssi}");
+    }, onError: (e) {
+      print('Error when start scan: $e');
+    }, onDone: () {
+      print('Done');
+    });
   }
 
 }
---
+++ lib/app/utils/ble/peripheral_tracing.dart
@@ -0,0 +1,43 @@
+import 'dart:typed_data';
+
+import 'package:blemulator/blemulator.dart';
+
+class PeripheralTracing extends SimulatedPeripheral {
+  PeripheralTracing(
+      {String id = "4C:99:4C:34:DE:76",
+      String name = "Peripheral",
+      String localName = "Peripheral"})
+      : super(
+            name: name,
+            id: id,
+            advertisementInterval: Duration(milliseconds: 800),
+            services: [
+              SimulatedService(
+                  uuid: "F000AA00-0451-4000-B000-000000000000",
+                  isAdvertised: true,
+                  characteristics: [
+                    SimulatedCharacteristic(
+                        uuid: "F000AA01-0451-4000-B000-000000000000",
+                        value: Uint8List.fromList([101, 254, 64, 12]),
+                        convenienceName: "IR Temperature Data"),
+                    SimulatedCharacteristic(
+                        uuid: "F000AA02-0451-4000-B000-000000000000",
+                        value: Uint8List.fromList([0]),
+                        convenienceName: "IR Temperature Config"),
+                    SimulatedCharacteristic(
+                        uuid: "F000AA03-0451-4000-B000-000000000000",
+                        value: Uint8List.fromList([50]),
+                        convenienceName: "IR Temperature Period"),
+                  ],
+                  convenienceName: "Temperature service")
+            ]) {
+              
+    scanInfo.localName = localName;
+  }
+
+  @override
+  Future<bool> onConnectRequest() async {
+    await Future.delayed(Duration(milliseconds: 200));
+    return super.onConnectRequest();
+  }
+}
