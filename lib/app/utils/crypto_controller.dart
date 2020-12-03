c6814caf7d304c4d9ec37baec4428c3aabfa8593
Felipe Vergara <favazcl@gmail.com>
2020-12-03 09:59:43 -0300

    create ephids system generator

--- lib/app/utils/crypto_controller.dart
+++ lib/app/utils/crypto_controller.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:covid_app/app/data/models/EphId.dart';
 import 'package:covid_app/app/utils/shared_preferences/shared_prefs_controller.dart';
@@ -12,7 +13,21 @@ class CryptoController extends GetxController {
   static const String IVE = 'F0Wlnt0zTvcB6bHW';
   final SharedPrefsController prefs =
       Get.put<SharedPrefsController>(SharedPrefsController());
-  EphId ephId = EphId();
+  EphId _ephId = EphId();
+  List<EphId> _ephIds = List<EphId>();
+
+  EphId get ephId => _ephId;
+  List<EphId> get ephIds => _ephIds;
+
+  set setEphId(EphId value) {
+    _ephId = value;
+    update();
+  }
+
+  set setEphIds(List<EphId> value) {
+    _ephIds = value;
+    update();
+  }
 
   @override
   void onInit() {
@@ -21,28 +36,70 @@ class CryptoController extends GetxController {
   }
 
   void init() {
-    _getCurrentEphId();
+    _getCurrentEphIds();
   }
 
-  void _getCurrentEphId() {
+  void _getCurrentEphIds() {
     if (prefs.ephids.isNullOrBlank) {
       _createEphId();
     } else {
       var decode = json.decode(prefs.ephids);
-      ephId = EphId.fromJson(decode);
+
+      for (var ephId in decode) {
+        List<int> listInt = List.castFrom(ephId['data']);
+        ephId['data'] = Uint8List.fromList(listInt);
+
+        EphId ephIdTmp = EphId.fromJson(ephId);
+        _ephIds.add(ephIdTmp);
+      }
+
+      _getCurrentEphId();
     }
   }
 
+  void _getCurrentEphId() {
+    DateTime currentDay = DateTime.now();
 
-  void _createEphId() async {
+    for (EphId ephId in ephIds) {
+      var parseDate = DateTime.fromMillisecondsSinceEpoch(ephId.createdAt);
+      int diffDays = currentDay.difference(parseDate).inDays;
+
+      bool isSame = (diffDays == 0 && parseDate.day == currentDay.day);
+
+      if (isSame) {
+        print('Son iguales');
+        _ephId = ephId;
+      } else {
+        print('No son iguales');
+        _createEphId();
+      }
+    }
+  }
+
+  void _createEphId() {
     try {
       final secretKey = SecureRandom(16);
       final dateTime = DateTime.now().millisecondsSinceEpoch;
 
-      ephId = EphId(createdAt: dateTime, data: secretKey.bytes);
+      _ephId = EphId(createdAt: dateTime, data: secretKey.bytes);
       var encode = json.encode(ephId);
 
-      prefs.ephids = encode.toString();
+      if (!prefs.ephids.isNullOrBlank) {
+        var listEphids = json.decode(prefs.ephids);
+        List newListEphids = List();
+
+        for (var item in listEphids) {
+          item = json.encode(item);
+          newListEphids.add(item);
+        }
+
+        newListEphids.add(encode);
+        prefs.ephids = newListEphids.toString();
+      } else {
+        List ephIds = List();
+        ephIds.add(encode);
+        prefs.ephids = ephIds.toString();
+      }
 
       /* TO DO.. Encriptar
       final key = Key.fromLength(32);
