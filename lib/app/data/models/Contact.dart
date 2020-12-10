ed5842219debe7e650af19fc60251aa8b2170e21
Felipe Vergara <favazcl@gmail.com>
2020-12-10 16:49:19 -0300

    add shared attr

--- lib/app/data/models/Contact.dart
+++ lib/app/data/models/Contact.dart
@@ -6,20 +6,23 @@ class Contact {
   int createdAt;
   List<Handshake> handshakes;
   int duration;
+  bool shared;
 
-  Contact({ this.id, this.createdAt, this.handshakes, this.duration });
+  Contact({ this.id, this.createdAt, this.handshakes, this.duration, this.shared });
 
   Contact.fromJson(Map<String, dynamic> json)
     : id = json['id'],
       createdAt = json['date'],
       handshakes = json['handshakes'],
-      duration = json['duration'];
+      duration = json['duration'],
+      shared = json['shared'];
 
   Map<String, dynamic> toJson() => {
     'id': id,
     'createdAt': createdAt,
     'handshakes': handshakes,
-    'duration': duration
+    'duration': duration,
+    'shared': shared
   };
 
 }
